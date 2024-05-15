c64d47827f0338ed92adc2bae156389da13cc01c
unknown <otaviosouzalu@gmail.com>
2024-05-15 15:36:31 -0300

    descomentando server

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 require('dotenv/config.js')
 const app = require('./src/app.js')
 
-const http = require('http');
+const https = require('https');
 const fs = require('fs');
 
-/*  const httpsOptions = {
+ const httpsOptions = {
     key: fs.readFileSync('/etc/letsencrypt/live/taiacupeba.com.br/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/taiacupeba.com.br/fullchain.pem')
-}; */
+}; 
 
-const server = http.createServer(/* httpsOptions */app);
+const server = https.createServer(httpsOptions,app);
 
 
 const PORT = 3333;
